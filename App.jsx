9a10f4282d509a34ff5fea5620e97d7a2fb9df9c
nithyamahe23 <165092074+nithyamahe23@users.noreply.github.com>
2024-11-06 09:42:41 -0500

    Create App

---
+++ App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Menu from './components/Menu'
+
+function App() {
+
+  return (
+    <>
+    <div className="mainDiv">
+            <header>
+            <h2>INVENTORY MANAGEMENT SYSTEM</h2>
+            </header>
+      <Menu/>
+      </div>
+    </>
+  )
+}
+
+export default App
